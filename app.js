1402ea159211e2c7406e8a1cfa4222855535e482
Yaroslav <kya.pk22.6.3@gmail.com>
2026-02-04 13:36:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,102 +5,110 @@ import { header } from "./header.js";
 import { toogle } from "./toogle.js";
 import { img } from "./img.js";
 
-
 document.addEventListener('DOMContentLoaded', function(){
-    const main   = {
+
+    const appConfig = {
         data() {
             return {
-                url:"https://affiliate.yanbasok.com",
-                user:{name:"", phone:"", email:"", date:"", auth:""},
+                url: "https://affiliate.yanbasok.com",
+                user: { id: null, name:"", phone:"", email:"", date:"", auth:"", type:"" },
+                title: "",
                 formData: {},
-                title:"",
-                date:"",
-                time:"",
             }
         },
-        watch:{
-            $route:function(){
+
+        watch: {
+            $route() {
                 this.init();
             }
         },
-        mounted:function(){
+
+        mounted() {
             this.init();
         },
-        methods:{
-            init(){
-                var self = this;
-                if(window.localStorage.getItem('user')) {
-                    self.user = JSON.parse(window.localStorage.getItem('user'));
-                    if(!self.user.id && self.user?.auth?.data){
-                        self.user.id = self.user.auth.data;
-                        window.localStorage.setItem('user', JSON.stringify(self.user));
+
+        methods: {
+
+            initUser() {
+                const stored = window.localStorage.getItem('user');
+                if(stored){
+                    this.user = JSON.parse(stored);
+                    if(!this.user.id && this.user?.auth?.data){
+                        this.user.id = this.user.auth.data;
+                        window.localStorage.setItem('user', JSON.stringify(this.user));
                     }
                 }
+            },
+
+            init() {
+                this.initUser();
 
                 router.isReady().then(() => {
-                    if(window.localStorage.getItem("user")){
-                         self.user = JSON.parse(window.localStorage.getItem("user"));
-                        if(!self.user.id && self.user?.auth?.data){
-                            self.user.id = self.user.auth.data;
-                            window.localStorage.setItem('user', JSON.stringify(self.user));
-                        }
-                        if(self.$route['path']=='/' && self.user.type=='admin'){
-                            self.page('/campaigns');
-                        }else if(['/campaigns','/campaign','/users','/user'].includes(self.$route['path']) && self.user.type!='admin'){
-                            self.page('/statistics');
-                        }else if(['/statistics','/payments','/sites'].includes(self.$route['path']) && self.user.type=='admin'){
-                            self.page('/campaigns');
-                        }else if(['/campaigns','/campaign','/users','/user', '/statistics','/payments','/sites' ].includes(self.$route['path']) && self.user.type!='admin'){
-                            self.page();
-                        }else if(!['/campaigns','/campaign','/users','/user', '/statistics','/payments','/sites' ].includes(self.$route['path']) && self.user.type!='admin'){
-                            self.page();    
+
+                    if(!this.user?.id){
+                        this.page('/');
+                        return;
                     }
-                }else{
-                    self.page('/');
-                }
+
+
+                    const pathSegment = this.$route.path.split('/')[1] || '';
+
+                    if(pathSegment === '' && this.user.type === 'admin'){
+                        this.page('/campaigns');
+                    }
+                    else if(['/campaigns','/campaign','/users','/user'].includes('/'+pathSegment) && this.user.type !== 'admin'){
+                        this.page('/statistics');
+                    }
+                    else if(['/statistics','/payments','/sites'].includes('/'+pathSegment) && this.user.type === 'admin'){
+                        this.page('/campaigns');
+                    }
+                    else {
+                        this.updateTitle();
+                    }
                 });
             },
-logout() {
-  this.user = { name:"", phone:"", email:"", date:"", auth:"" };
-  window.localStorage.removeItem('user');
-  this.page('/');
-},
-            scrollTop(){
-                setTimeout(function(){
-                    window.scroll({
-                        top: 0,
-                        behavior: 'smooth'
-                    });
-                },50);
-            },
-            scrollButton(){
-                setTimeout(function(){
-                    window.scroll({
-                        top: 1000,
-                        behavior: 'smooth'
-                    });
-                },50);
-            },
-            page: function (path=""){
-                this.$router.replace(path);
-                this.title=this.$route['name'];
-                document.title=this.$route['name'];
-            },
-            toFormData:function(obj){
-                var fd = new FormData();
 
-                for(var x in obj){
-                    if(typeof obj[x]==='object' && x != 'img' && x != 'copy'){
-                        for(var y in obj[x]){
-                            if(typeof obj[x]==='object'){
-                                for(var z in obj[x][y]){
-                                    fd.append(x+'['+y+']['+z+']', obj[x][y][z]);
+
+            logout() {
+                this.user = { id:null, name:"", phone:"", email:"", date:"", auth:"", type:"" };
+                window.localStorage.removeItem('user');
+                this.page('/');
+            },
+
+            scrollTop(){
+                setTimeout(() => window.scroll({ top: 0, behavior: 'smooth' }), 50);
+            },
+
+            scrollButton(){
+                setTimeout(() => window.scroll({ top: 1000, behavior: 'smooth' }), 50);
+            },
+
+            page(path=""){
+                this.$router.replace(path).then(() => {
+                    this.updateTitle();
+                });
+            },
+
+            updateTitle() {
+                this.title = this.$route.name || '';
+                document.title = this.title;
+            },
+
+            toFormData(obj){
+                const fd = new FormData();
+
+                for(const x in obj){
+                    if(typeof obj[x] === 'object' && x !== 'img' && x !== 'copy'){
+                        for(const y in obj[x]){
+                            if(typeof obj[x][y] === 'object'){
+                                for(const z in obj[x][y]){
+                                    fd.append(`${x}[${y}][${z}]`, obj[x][y][z]);
                                 }
-                            }else{
-                                fd.append(x+'['+y+']', obj[x][y]);
+                            } else {
+                                fd.append(`${x}[${y}]`, obj[x][y]);
                             }
                         }
-                    }else if(x !='copy'){
+                    } else if(x !== 'copy'){
                         fd.append(x, obj[x]);
                     }
                 }
@@ -110,28 +118,12 @@ logout() {
         }
     };
 
-    var app = Vue.createApp(main)
-    .component('img', img)
-    .component('toogle', toogle)
-    .component('Header',header)
-    .component('popup',popup)
-    .component('msg',msg)
-    app.use(router)
-    .mount('#content')
+    const app = Vue.createApp(appConfig)
+        .component('img', img)
+        .component('toogle', toogle)
+        .component('Header', header)
+        .component('popup', popup)
+        .component('msg', msg);
+
+    app.use(router).mount('#content');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
